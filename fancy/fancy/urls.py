40646bd41caa171275c0764ef7bfccf2318dd93d
CatalinaBrinza <brinza_catalina@yahoo.com>
2016-01-16 11:50:48 +0200

    Add base template and home view

--- fancy/fancy/urls.py
+++ fancy/fancy/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from pansy.views.home import HomeView
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'fancy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    
+    url(r'^$', HomeView.as_view(), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
