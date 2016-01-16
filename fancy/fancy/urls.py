8614bd4cc04a27b62eac9735383722e6658f001d
Dayanna Amegica <dayanna.amegica@gmail.com>
2016-01-16 13:04:23 +0200

    Fix url

--- fancy/fancy/urls.py
+++ fancy/fancy/urls.py
@@ -8,8 +8,9 @@ from fancy.settings import STATIC_ROOT, STATIC_URL
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^signin/', authentication.signin),
-    url(r'^signup/', authentication.signup),
+    url(r'^signin/', authentication.signin, name="signin"),
+    url(r'^signup/', authentication.signup, name="signup"),
+    url(r'^signout/', authentication.signout, name="signout")
 ]
 
 urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
