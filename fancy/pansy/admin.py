83a2c9219f75e4ec3f44dc5705627cf3689a5643
dan-ghita <dan_ghita_95@yahoo.com>
2016-01-16 11:48:11 +0200

    Adds Tag and Comment models

--- fancy/pansy/admin.py
+++ fancy/pansy/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from pansy.models import photo
+from pansy.models import tag
+from pansy.models import comment
 # Register your models here.
 
 admin.site.register(photo.Photo)
+admin.site.register(tag.Tag)
+admin.site.register(comment.Comment)
--- fancy/pansy/models/photo.py
+++ fancy/pansy/models/photo.py
@@ -2,11 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Photo(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=50, blank=False)
     description = models.TextField()
-    path = models.TextField()
-    date_added = models.DateTimeField(
-        auto_now_add=True)
+    path = models.CharField(max_length=100, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
     public = models.BooleanField()
 
     # tags
